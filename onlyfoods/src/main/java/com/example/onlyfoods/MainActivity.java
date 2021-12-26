f415ba0d10805152248891fb641aadb0e7651fba
Fitri Makmor <mcfitri99@gmail.com>
2021-12-26 16:39:24 +0800

    first komito

---
+++ onlyfoods/src/main/java/com/example/onlyfoods/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.onlyfoods;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NavUtils;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavHostFragment host =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.NHFMain);
+        NavController navController = host.getNavController();
+
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        setupBottomNavMenu(navController);
+
+//        DrawerLayout drawerLayout = findViewById(R.id.DLMain);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawerLayout.addDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        setupNavMenu(navController);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_bottom, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        try {
+//            Navigation.findNavController(this, R.id.NHFMain).navigate(item.getItemId());
+//            Navigation.findNavController(this, R.id.NHFMain).navigate(R.id.DestHome);
+            Navigation.findNavController(this, R.id.NHFMain).navigateUp();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setupBottomNavMenu(NavController navController) {
+        BottomNavigationView bottomNav = findViewById(R.id.BottomNavView);
+        NavigationUI.setupWithNavController(bottomNav, navController);
+    }
+
+//    private void setupNavMenu(NavController navController){
+//        NavigationView sideNav = findViewById(R.id.sideNav);
+//        NavigationUI.setupWithNavController(sideNav, navController);
+//    }
+
+}
