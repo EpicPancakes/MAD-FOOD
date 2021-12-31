1946afc89e443bcb54d65b6665efa792e698eb84
Fitri Makmor <mcfitri99@gmail.com>
2021-12-31 18:27:55 +0800

    added the tab layout, view pager and recycler view for the profile view

--- onlyfoods/src/main/java/com/example/onlyfoods/MyProfileFragment.java
+++ onlyfoods/src/main/java/com/example/onlyfoods/MyProfileFragment.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +30,11 @@ public class MyProfileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
+    // ViewPager implementation to slide between recent places and reviews
+    TabLayout tabLayout;
+    ViewPager2 viewPager;
 
     public MyProfileFragment() {
         // Required empty public constructor
@@ -58,41 +67,59 @@ public class MyProfileFragment extends Fragment {
         }
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_my_profile, container, false);
+        addFragment(view);
+        return view;
+    }
+
+    private void addFragment(View view){
+        tabLayout = view.findViewById(R.id.TLEditProfile);
+        viewPager = view.findViewById(R.id.VPEditProfile);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager(), getLifecycle());
+        adapter.addFragment(new RecentPlacesFragment(), "Recent Places");
+        adapter.addFragment(new ReviewsFragment(), "Reviews");
+        viewPager.setAdapter(adapter);
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            if(position == 0)
+                tab.setText("Recent Places");
+            else
+                tab.setText("Reviews");
+        }).attach();
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
-        Button BtnCat = view.findViewById(R.id.BtnCat);
-        View.OnClickListener OCLCat = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.DestCat);
-            }
-        };
-        BtnCat.setOnClickListener(OCLCat);
-
-        Button BtnDog = view.findViewById(R.id.BtnDog);
-        View.OnClickListener OCLDog = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.NextToDog);
-            }
-        };
-        BtnDog.setOnClickListener(OCLDog);
-
-        Button BtnHedgehog = view.findViewById(R.id.BtnHedgehog);
-        View.OnClickListener OCLHedgehog = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.NextToHedgehog);
-            }
-        };
-        BtnHedgehog.setOnClickListener(OCLHedgehog);
+//        Button BtnCat = view.findViewById(R.id.BtnCat);
+//        View.OnClickListener OCLCat = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Navigation.findNavController(view).navigate(R.id.DestCat);
+//            }
+//        };
+//        BtnCat.setOnClickListener(OCLCat);
+//
+//        Button BtnDog = view.findViewById(R.id.BtnDog);
+//        View.OnClickListener OCLDog = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Navigation.findNavController(view).navigate(R.id.NextToDog);
+//            }
+//        };
+//        BtnDog.setOnClickListener(OCLDog);
+//
+//        Button BtnHedgehog = view.findViewById(R.id.BtnHedgehog);
+//        View.OnClickListener OCLHedgehog = new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Navigation.findNavController(view).navigate(R.id.NextToHedgehog);
+//            }
+//        };
+//        BtnHedgehog.setOnClickListener(OCLHedgehog);
 
         Button BtnEditProfile = view.findViewById(R.id.BtnEditProfile);
         View.OnClickListener OCLEditProfile = new View.OnClickListener() {
---
+++ onlyfoods/src/main/java/com/example/onlyfoods/MyReviewsRecyclerViewAdapter.java
@@ -0,0 +1,62 @@
+package com.example.onlyfoods;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.onlyfoods.placeholder.PlaceholderContent.PlaceholderItem;
+import com.example.onlyfoods.databinding.FragmentReviewsBinding;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyReviewsRecyclerViewAdapter extends RecyclerView.Adapter<MyReviewsRecyclerViewAdapter.ViewHolder> {
+
+    private final List<PlaceholderItem> mValues;
+
+    public MyReviewsRecyclerViewAdapter(List<PlaceholderItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(FragmentReviewsBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public PlaceholderItem mItem;
+
+        public ViewHolder(FragmentReviewsBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.itemNumber2;
+            mContentView = binding.content2;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
