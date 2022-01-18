d7127c404f2c2865be38d23d785657f7c5cbd4fa
LeyhangCheng <86043945+LeyhangCheng@users.noreply.github.com>
2022-01-18 17:13:13 +0800

    uncommeneted setupnavmenu

--- onlyfoods/src/main/java/com/example/onlyfoods/MainActivity.java
+++ onlyfoods/src/main/java/com/example/onlyfoods/MainActivity.java
@@ -72,9 +72,10 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(bottomNav, navController);
     }
 
-//    private void setupNavMenu(NavController navController){
-//        NavigationView sideNav = findViewById(R.id.sideNav);
-//        NavigationUI.setupWithNavController(sideNav, navController);
-//    }
+    // delete this
+    private void setupNavMenu(NavController navController){
+        //NavigationView sideNav = findViewById(R.id.sideNav);
+        //NavigationUI.setupWithNavController(sideNav, navController);
+    }
 
 }
