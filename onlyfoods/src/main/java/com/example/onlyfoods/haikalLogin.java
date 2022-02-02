69435574ceac1e3c07c7e8418949491ca3d67263
Arif Haikal <arifhaikal@gmail.com>
2022-02-02 17:31:47 +0800

    added format checking for logging in and account creation

--- onlyfoods/src/main/java/com/example/onlyfoods/haikalLogin.java
+++ onlyfoods/src/main/java/com/example/onlyfoods/haikalLogin.java
@@ -5,16 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class haikalLogin extends AppCompatActivity {
+
+    TextView TVRegister;
+    EditText LoginEmail, LoginPassword;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_haikal_login);
 
-        TextView TVRegister = (TextView)findViewById(R.id.TVRegister);
+        TVRegister = (TextView)findViewById(R.id.TVRegister);
 
         TVRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +31,37 @@ public class haikalLogin extends AppCompatActivity {
 
             }
         });
+
+        LoginEmail = findViewById(R.id.ETLoginEmail);
+        LoginPassword = findViewById(R.id.ETLoginPassword);
+        btnLogin = findViewById(R.id.BtnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkCredentials();
+            }
+        });
+    }
+
+
+    private void checkCredentials() {
+        String email = LoginEmail.getText().toString();
+        String password = LoginPassword.getText().toString();
+
+        if (email.isEmpty() || !email.contains("@")){
+            showError(LoginEmail, "Invalid email");
+        }
+        else if (password.isEmpty() || password.length()<8){
+            showError(LoginPassword, "Password must be 8 characters or more");
+        }
+        else{
+            Toast.makeText(this, "Call Login Method", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void showError(EditText ET, String s) {
+        ET.setError(s);
+        ET.requestFocus();
     }
 }
--- onlyfoods/src/main/java/com/example/onlyfoods/haikalRegistration.java
+++ onlyfoods/src/main/java/com/example/onlyfoods/haikalRegistration.java
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class haikalRegistration extends AppCompatActivity {
+
+    private EditText RegisterUsername, RegisterEmail, RegisterPassword, ConfirmPassword;
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +30,47 @@ public class haikalRegistration extends AppCompatActivity {
 
             }
         });
+
+        RegisterUsername = findViewById(R.id.ETRegisterUsername);
+        RegisterEmail = findViewById(R.id.ETRegisterEmail);
+        RegisterPassword = findViewById(R.id.ETRegisterPassword);
+        ConfirmPassword = findViewById(R.id.ETConfirmPassword);
+        btnRegister = findViewById(R.id.BtnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkCredentials();
+            }
+        });
+    }
+
+    private void checkCredentials() {
+        String username = RegisterUsername.getText().toString();
+        String email = RegisterEmail.getText().toString();
+        String password = RegisterPassword.getText().toString();
+        String confirmPassword = ConfirmPassword.getText().toString();
+
+        if (username.isEmpty() || username.length()<5){
+            showError(RegisterUsername, "Username must be 5 characters or more");
+        }
+        else if (email.isEmpty() || !email.contains("@")){
+            showError(RegisterEmail, "Invalid email");
+        }
+        else if (password.isEmpty() || password.length()<8){
+            showError(RegisterPassword, "Password must be 8 characters or more");
+        }
+        else if (confirmPassword.isEmpty() || !confirmPassword.equals(password)){
+            showError(ConfirmPassword, "Password does not match");
+        }
+        else{
+            Toast.makeText(this, "Call Registration Method", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void showError(EditText ET, String s) {
+        ET.setError(s);
+        ET.requestFocus();
     }
 }
