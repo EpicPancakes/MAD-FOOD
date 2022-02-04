40ebc6fdb65b0b062b1a91df32bd4dbe9c121fbf
Fitri Makmor <mcfitri99@gmail.com>
2022-02-05 01:46:01 +0800

    add logout operation

--- onlyfoods/src/main/java/com/example/onlyfoods/Fragments/MyProfileFragment.java
+++ onlyfoods/src/main/java/com/example/onlyfoods/Fragments/MyProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.onlyfoods.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +24,7 @@ import com.example.onlyfoods.Models.Backdrop;
 import com.example.onlyfoods.Models.ProfileImage;
 import com.example.onlyfoods.Models.User;
 import com.example.onlyfoods.R;
+import com.example.onlyfoods.haikalLogin;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.FirebaseAuth;
@@ -231,6 +233,16 @@ public class MyProfileFragment extends Fragment {
             }
         };
         BtnRecommendations.setOnClickListener(OCLRecommendations);
+
+        Button BtnLogout = view.findViewById(R.id.BTNLogout);
+        BtnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(view.getContext(), haikalLogin.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
