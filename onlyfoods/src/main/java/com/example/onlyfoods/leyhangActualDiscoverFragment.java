8a11d0ac64ff6a0484bd3102ccf639736d5f4571
LeyhangCheng <86043945+LeyhangCheng@users.noreply.github.com>
2022-02-05 14:19:28 +0800

    added the recyclerview and clean up

--- onlyfoods/src/main/java/com/example/onlyfoods/leyhangActualDiscoverFragment.java
+++ onlyfoods/src/main/java/com/example/onlyfoods/leyhangActualDiscoverFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link leyhangActualDiscover#newInstance} factory method to
+ * Use the {@link leyhangActualDiscoverFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class leyhangActualDiscover extends Fragment {
+public class leyhangActualDiscoverFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class leyhangActualDiscover extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public leyhangActualDiscover() {
+    public leyhangActualDiscoverFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class leyhangActualDiscover extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment leyhangActualDiscover.
+     * @return A new instance of fragment leyhangActualDiscoverFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static leyhangActualDiscover newInstance(String param1, String param2) {
-        leyhangActualDiscover fragment = new leyhangActualDiscover();
+    public static leyhangActualDiscoverFragment newInstance(String param1, String param2) {
+        leyhangActualDiscoverFragment fragment = new leyhangActualDiscoverFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
