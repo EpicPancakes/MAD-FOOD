938a474cda3cc80d5e5fe57ca422c48decaa5ea0
LeyhangCheng <86043945+LeyhangCheng@users.noreply.github.com>
2022-02-05 14:44:17 +0800

    latest adapter java file discover

--- onlyfoods/src/main/java/com/example/onlyfoods/Adapters/DiscoverRecyclerViewAdapter.java
+++ onlyfoods/src/main/java/com/example/onlyfoods/Adapters/DiscoverRecyclerViewAdapter.java
@@ -1,16 +1,51 @@
 package com.example.onlyfoods.Adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class DiscoverRecyclerViewAdapter {
+import com.example.onlyfoods.R;
+
+public class DiscoverRecyclerViewAdapter extends RecyclerView.Adapter<DiscoverRecyclerViewAdapter.MyViewHolder>{
+
+    // Testing Purpose
+    String[] list;
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.leyhang_discover_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.TVDiscoverName.setText(list[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.length;
+    }
 
     static class MyViewHolder extends RecyclerView.ViewHolder{
 
+        TextView TVDiscoverName, TVDiscoverAddress, TVDiscoverOpening;
+        Button BtnDiscoverSaved;
+        ImageView IVFilterPic;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            TVDiscoverName = itemView.findViewById(R.id.TVDiscoverName);
+            TVDiscoverAddress = itemView.findViewById(R.id.TVDiscoverAddress);
+            TVDiscoverOpening = itemView.findViewById(R.id.TVDiscoverOpening);
+            BtnDiscoverSaved = itemView.findViewById(R.id.BtnDiscoverSaved);
+            IVFilterPic = itemView.findViewById(R.id.IVFilterPic);
         }
     }
 }
